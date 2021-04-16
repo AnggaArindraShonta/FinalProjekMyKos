e394b8014d1de9182dd3f8546e50da7015bbbab1
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-16 17:12:11 +0700

    use ViewBinding navigate to SignInActivity

--- app/src/main/java/com/example/mykos/ui/ExploreActivity.java
+++ app/src/main/java/com/example/mykos/ui/ExploreActivity.java
@@ -1,16 +1,30 @@
 package com.example.mykos.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import com.example.mykos.R;
+import com.example.mykos.databinding.ActivityExploreBinding;
+import com.example.mykos.ui.sigin.SignInActivity;
 
 public class ExploreActivity extends AppCompatActivity {
+
+    private ActivityExploreBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_explore);
+        binding = ActivityExploreBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnExploreNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
