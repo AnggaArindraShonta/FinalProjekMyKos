d486b07a210ead942f5161cfcca81536f0fd4d83
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-16 16:39:30 +0700

    new update init viewmodel, subscribe to observers, method login, use mvvm style

--- app/src/main/java/com/example/mykos/ui/sigin/SignInActivity.java
+++ app/src/main/java/com/example/mykos/ui/sigin/SignInActivity.java
@@ -1,14 +1,14 @@
 package com.example.mykos.ui.sigin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mykos.R;
 import com.example.mykos.ui.HomeActivity;
@@ -19,7 +19,9 @@ public class SignInActivity extends AppCompatActivity {
 
     private MaterialEditText etEmail;
     private MaterialEditText etPassword;
-    private ImageButton btnsignin;
+    private ImageButton btnSignIn;
+
+    private SignInViewModel signInViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,44 +30,52 @@ public class SignInActivity extends AppCompatActivity {
 
         etEmail = findViewById(R.id.email);
         etPassword = findViewById(R.id.password);
-        btnsignin = findViewById(R.id.btnsignin);
+        btnSignIn = findViewById(R.id.btnsignin);
 
-        login();
-    }
-    private void login() {
-        btnsignin.setOnClickListener(new View.OnClickListener() {
+        subscribeToObservers();
+
+        initViewModel();
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (etEmail.getText() == null || etPassword.getText() == null) return;
+                login();
+            }
+        });
+        login();
+    }
 
-                final String email = etEmail.getText().toString();
-                final String password = etPassword.getText().toString();
+    private void initViewModel() {
+        SharedPrefManager sharedPrefManager = new SharedPrefManager(this);
+        ViewModelProvider.Factory factory = new SignInViewModelFactory(sharedPrefManager);
+        signInViewModel = new ViewModelProvider(this, factory).get(SignInViewModel.class);
+    }
 
-                if (email.isEmpty() || password.isEmpty()){
-                    Toast.makeText(SignInActivity.this, "username dan password tidak boleh kosong :)", Toast.LENGTH_SHORT).show();
-                }else{
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            String spEmail = sharedPrefManager.getUsername();
-                            String spPassword = sharedPrefManager.getPassword();
-
-                            Log.d("email","email" + email);
-                            Log.d("password","pass" + password);
-
-                            if (email.equals(spEmail) && password.equals(spPassword)){
-                                Intent i = new Intent(SignInActivity.this, HomeActivity.class);
-                                sharedPrefManager.saveIsLogin(true);
-                                finishAffinity();
-                                startActivity(i);
-                            }else {
-                                Toast.makeText(SignInActivity.this, "username dan password salah :)", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }, 3000);
+    private void subscribeToObservers() {
+        signInViewModel.observeIsLogin().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isSuccessLogin) {
+                if (isSuccessLogin){
+                    Intent i = new Intent(SignInActivity.this, HomeActivity.class);
+                    finishAffinity();
+                    startActivity(i);
+                }else {
+                    Toast.makeText(SignInActivity.this, "username dan password salah :)", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private void login() {
+        if (etEmail.getText() == null || etPassword.getText() == null) return;
+
+        final String email = etEmail.getText().toString();
+        final String password = etPassword.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(SignInActivity.this, "username dan password tidak boleh kosong :)", Toast.LENGTH_SHORT).show();
+        }else{
+            signInViewModel.login(email, password);
+        }
     }
 }
