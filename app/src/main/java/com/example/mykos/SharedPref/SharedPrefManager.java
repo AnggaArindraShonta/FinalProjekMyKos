000c3e18a6279e6e070f1c7dcdeca3afdfa956a2
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-16 16:36:50 +0700

    optimize SharedPrefManager

--- app/src/main/java/com/example/mykos/SharedPref/SharedPrefManager.java
+++ app/src/main/java/com/example/mykos/SharedPref/SharedPrefManager.java
@@ -4,36 +4,33 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SharedPrefManager {
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
-
+    SharedPreferences pref;
 
     public SharedPrefManager(Context context) {
-        sharedPreferences = context.getSharedPreferences("latihan_shared_preference",context.MODE_PRIVATE);
-        editor =sharedPreferences.edit();
+        pref = context.getSharedPreferences("latihan_shared_preference", Context.MODE_PRIVATE);
     }
 
     public void saveString(String value) {
-        editor.putString("ini_string",value);
-        editor.commit();
+        pref.edit().putString("ini_string", value).apply();
     }
 
     public String getString() {
-        return sharedPreferences.getString("ini_string", "");
-    }
-    public String getUsername(){
-        return sharedPreferences.getString("sp_email", "anggaarindra@gmail.com");
+        return pref.getString("ini_string", "");
     }
 
-    public String getPassword(){
-        return sharedPreferences.getString("sp_password", "0423");
+    public String getUsername() {
+        return pref.getString("sp_email", "anggaarindra@gmail.com");
     }
 
-    public void saveIsLogin(Boolean value){
-        editor.putBoolean("sp_islogin",value);
-        editor.commit();
+    public String getPassword() {
+        return pref.getString("sp_password", "0423");
     }
+
+    public void saveIsLogin(Boolean value) {
+        pref.edit().putBoolean("sp_islogin", value).apply();
+    }
+
     public boolean getIsLogin() {
-        return sharedPreferences.getBoolean("sp_islogin", false);
+        return pref.getBoolean("sp_islogin", false);
     }
 }
