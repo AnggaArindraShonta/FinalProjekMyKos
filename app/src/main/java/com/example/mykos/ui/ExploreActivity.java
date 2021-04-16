a937f8bb68ee955ccce80ef238085ea86589780f
AnggaArindraShonta <angga.shonta@students.amikom.ac.id>
2021-04-16 20:10:27 +0700

    home dan detail

--- app/src/main/java/com/example/mykos/ui/ExploreActivity.java
+++ app/src/main/java/com/example/mykos/ui/ExploreActivity.java
@@ -2,15 +2,28 @@ package com.example.mykos.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.mykos.R;
 
 public class ExploreActivity extends AppCompatActivity {
 
+    ImageButton button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
+
+        button = findViewById(R.id.flatsplash);
+        button.setOnClickListener(v -> {
+            Intent i = new Intent(ExploreActivity.this, HomeActivity.class);
+            finishAffinity();
+            startActivity(i);
+        });
+
     }
 }
