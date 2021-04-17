a9ff129a740bd068f7c322a2cbf43292429d3e06
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-17 11:03:58 +0700

    cleanup code

--- app/src/main/java/com/example/mykos/ui/ExploreActivity.java
+++ app/src/main/java/com/example/mykos/ui/ExploreActivity.java
@@ -1,18 +1,12 @@
 package com.example.mykos.ui;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mykos.databinding.ActivityExploreBinding;
-import com.example.mykos.ui.sigin.SignInActivity;
 
 public class ExploreActivity extends AppCompatActivity {
 
@@ -26,13 +20,10 @@ public class ExploreActivity extends AppCompatActivity {
         binding = ActivityExploreBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnExploreNow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(ExploreActivity.this, HomeActivity.class);
-                finishAffinity();
-                startActivity(i);
-            }
+        binding.btnExploreNow.setOnClickListener(v -> {
+            Intent i = new Intent(ExploreActivity.this, HomeActivity.class);
+            finishAffinity();
+            startActivity(i);
         });
     }
 }
