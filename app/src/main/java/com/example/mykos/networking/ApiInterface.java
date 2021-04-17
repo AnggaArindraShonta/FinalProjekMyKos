686d339252533fcdc0556c5b6776afd10a9074f4
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-04-17 11:31:29 +0700

    tambah getRecommendedSpace

--- app/src/main/java/com/example/mykos/networking/ApiInterface.java
+++ app/src/main/java/com/example/mykos/networking/ApiInterface.java
@@ -2,6 +2,8 @@ package com.example.mykos.networking;
 
 import com.example.mykos.model.Kos;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -13,4 +15,7 @@ public interface ApiInterface {
 
     @GET("movie/{id}")
     Call<Kos> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+
+    @GET("recommended-spaces")
+    Call<List<Kos>> getRecommendedSpace();
 }
